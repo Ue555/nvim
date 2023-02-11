da55c189c208ac69e4f1a94f2cbd5b794588b53f
Ue555 <122501501+Ue555@users.noreply.github.com>
2023-02-12 02:57:17 +0900

    initの設定の修正とプラグインの整理

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
+--lua/plugins.luaの読込み
 require "plugins"
+
+--設定ファイルの自動化
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { "plugins.lua" },
+  command = "PackerCompile",
+})
+
+--クリップボード設定
+vim.api.nvim_set_option('clipboard', 'unnamedplus')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,24 +2,36 @@ vim.cmd.packadd "packer.nvim"
 
 require("packer").startup(function()
   -- 起動時に読み込むプラグインは名前を書くだけです
-  use "tpope/vim-fugitive"
-  use "tpope/vim-repeat"
-
+ 
   -- opt オプションを付けると遅延読み込みになります。
   -- この場合は opt だけで読み込む契機を指定していないため、
   -- packadd コマンドを叩かない限り読み込まれることはありません。
   use { "wbthomason/packer.nvim", opt = true }
-  -- packer.nvim 自体を遅延読み込みにする理由はまた後ほど。
 
-  -- コマンドを叩いたときに読み込む。
+  -- Gitを操作するプラグイン
+  use "tpope/vim-fugitive"
+
+  -- Gitのコミットメッセージを表示するプラグイン(コマンドを叩いたときに読み込む)
   use { "rhysd/git-messenger.vim", opt = true, cmd = { "GitMessenger" } }
+ 
+  -- .によるコマンド繰り返し機能をより強力にしてくれるプラグイン
+  use "tpope/vim-repeat"
+  -- coc.nvimのプラグイン
+  use { 'neoclide/coc.nvim', branch='release' }
+    use 'lervag/vimtex'
 
-  -- 特定のイベントで読み込む
+  --[[ On new systems install COC packages via
+        CocInstall coc-vimtex coc-texlab
+  --]]
+
+  -- QuickFix ウィンドウを内容に応じて伸縮してくれるプラグイン
+  -- QuickFixへGit の差分を分かり易く表示(特定のイベントで読み込む)
   use { "tpope/vim-unimpaired", opt = true, event = { "FocusLost", "CursorHold" } }
 
   -- 特定のファイルタイプのファイルを開いたら読み込む
   use { "fatih/vim-go", opt = true, ft = { "go" } }
-
+  
+  -- 開いたフォールドと閉じたフォールド、およびネストされたフォールドを循環させる機能を提供するプラグイン
   -- 特定のキーを叩いたら読み込む
   -- この例ではノーマルモードの <CR> にマッピングしていますが、
   -- モードを指定する場合はテーブルを入れ子にします。
@@ -33,27 +45,13 @@ require("packer").startup(function()
     keys = { "<CR>" },
   }
 
+  -- フォントを変更せずに文字の表示を変更することができるプラグイン
   -- 特定の VimL 関数を呼ぶと読み込む
   -- この例だと、任意の場所で Artify("hoge", "bold") のように呼び出された時に、
   -- このプラグインも読み込まれます。
   use { "sainnhe/artify.vim", opt = true, fn = { "Artify" } }
 
-  -- 実は opt = true は省略できます。読み込む契機（この例では cmd）を指定すると、
-  -- 自動的に遅延読み込みとみなされます。
-  use {
-    "npxbr/glow.nvim",
-    cmd = { "Glow", "GlowInstall" },
-    -- run オプションを指定すると、インストール時・更新時に
-    -- 実行するコマンドを指定できます。
-    run = [[:GlowInstall]],
-    -- 先頭に : がついていないなら bash -c "..." で実行されます。
-    -- run = [[npm install]],
-    -- 関数も指定可能です。
-    -- run = function()
-    --   vim.cmd.GlowInstall()
-    -- end,
-  }
-
+  -- TODO 利用用途がわからないプラグイン
   -- 条件が真の時のみ読み込みます。条件は起動時に毎回判定されます。
   use {
     "thinca/vim-fontzoom",
@@ -64,10 +62,12 @@ require("packer").startup(function()
     -- end,
   }
 
+  -- Exコマンドの出力をバッファに表示するプラグイン
   -- 依存関係も管理できます。vim-prettyprint は capture.vim に依存しています。
   use {
     "tyru/capture.vim",
     requires = {
+      -- Vimの変数を整形して出力してくれるプラグイン
       { "thinca/vim-prettyprint" },
     },
   }
