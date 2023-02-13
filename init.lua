047bff06f70828611094309a8ef84235ce215385
Ue555 <122501501+Ue555@users.noreply.github.com>
2023-02-13 10:43:32 +0900

    設定ファイルの修正
    vim利用時の設定を適用する

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 --lua/plugins.luaの読込み
+
+--プラグインの設定
 require "plugins"
 
 --設定ファイルの自動化
@@ -6,6 +8,9 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = { "plugins.lua" },
   command = "PackerCompile",
 })
+
+--デフォルトの設定(vim利用時の設定)
+require "base"
 
 --クリップボード設定
 vim.api.nvim_set_option('clipboard', 'unnamedplus')
